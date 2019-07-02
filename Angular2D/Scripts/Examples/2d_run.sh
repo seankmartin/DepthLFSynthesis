b9f3179b5b5e3996a9edf5ddc4ee2af5610cec73
sean <martins7@tcd.ie>
2019-07-02 13:13:17 +0100

    Updated run scripts

--- Angular2D/Scripts/Examples/2d_run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ../
-
-./run_models.sh "--tag 2D_model_small_fixed --pretrained EDSR_x2.pt --n 10 --cfg small.ini"
---
+++ Angular2D/Scripts/Examples/run_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd ../
+
+./run_models.sh "--tag Tiny --n 15 --prompt --cfg /home/sean/repos/DepthLFSynthesis/Angular2D/PythonCode/config/small.ini -ac"
+# "--tag Unseen --n 5 --cfg /home/sean/repos/DepthLFSynthesis/Angular2D/PythonCode/config/head.ini -ac" \
+# "--tag Heart --n 5 --cfg /home/sean/repos/DepthLFSynthesis/Angular2D/PythonCode/config/main.ini -ac" \
+
