c5aa6d2c20c68a5578d757585c60d19420979ed8
sean <martins7@tcd.ie>
2019-07-02 17:01:38 +0100

    Further update script for saving images

--- PythonHelpers/print_sample_h5_vals.py
+++ PythonHelpers/print_sample_h5_vals.py
@@ -5,12 +5,12 @@ import numpy as np
 
 import common
 
-def main(file_location):
+def main(file_location, out_loc):
     with h5py.File(file_location, 'r') as f:
         for i in range(3):
             for j in range(64):
                 im_out_location = os.path.join(
-                    "/home/sean/lf_datasets/unseentest/{}/gt{}.png".format(i, j))
+                    out_loc, "{}/gt{}.png".format(i, j))
                 data = np.swapaxes(f["train"]["images"][i][j], 0, 2)
                 data = np.swapaxes(data, 0, 1)
                 common.save_numpy_image(
@@ -18,7 +18,7 @@ def main(file_location):
                     location=im_out_location
                 )
                 im_out_location = os.path.join(
-                    "/home/sean/lf_datasets/unseentest/{}/warp{}.png".format(i, j))
+                    out_loc, "{}/warp{}.png".format(i, j))
                 data = np.swapaxes(f["train"]["warped"][i][j], 0, 2)
                 data = np.swapaxes(data, 0, 1)
                 common.save_numpy_image(
@@ -28,6 +28,7 @@ def main(file_location):
 
 if __name__ == "__main__":
     file_location = os.path.join(
-        "/home/sean/lf_datasets/test_sets/", "head_set_new.h5"
+        "/home/sean/lf_datasets/test_sets/", "test_new.h5"
     )
-    main(file_location)
+    out_loc = "/home/sean/lf_datasets/seentest/"
+    main(file_location, out_loc)
