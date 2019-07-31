4a1f94c278282b8347f67ccc058be22fa03ffc51
Sean Martin <martins7@tcd.ie>
2019-07-31 10:42:58 +0100

    Clearer spacing

--- PythonHelpers/print_h5_summary.py
+++ PythonHelpers/print_h5_summary.py
@@ -2,24 +2,27 @@ import h5py
 import os
 import argparse
 
-def walk_dict(d,depth=0):
-    for k,v in sorted(d.items(),key=lambda x: x[0]):
-        spaces = ("  ")*depth
+
+def walk_dict(d, depth=0):
+    for k, v in sorted(d.items(), key=lambda x: x[0]):
+        spaces = ("  ") * depth
         if hasattr(v, "items"):
             print(spaces + ("%s" % k))
-            walk_dict(v,depth+1)
+            walk_dict(v, depth + 1)
         else:
             print(spaces + "%s %s" % (k, v))
 
+
 def get_attrs(d, depth=0):
-    for k,v in sorted(d.items(),key=lambda x: x[0]):
-        spaces = ("  ")*depth
+    for k, v in sorted(d.items(), key=lambda x: x[0]):
+        spaces = ("  ") * depth
         if len(v.attrs) is not 0:
             print("{}{} has attrs {}".format(
-                    spaces, k, list(v.attrs.items())
-                ))
+                spaces, k, list(v.attrs.items())
+            ))
         if hasattr(v, "items"):
-            get_attrs(v, depth=depth+1)
+            get_attrs(v, depth=depth + 1)
+
 
 def main(file_location):
     with h5py.File(file_location, 'r', libver='latest') as f:
@@ -28,10 +31,10 @@ def main(file_location):
         get_attrs(f)
         print()
 
+
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
         description='Process modifiable parameters from command line')
     PARSER.add_argument("--loc", type=str, help="h5 location")
     ARGS, UNPARSED = PARSER.parse_known_args()
     main(ARGS.loc)
-
